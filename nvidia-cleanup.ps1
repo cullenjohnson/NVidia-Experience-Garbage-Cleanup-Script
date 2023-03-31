08966a9353092e2c08d2af50ddd12acfedbd90dd
Cullen Johnson <cullen.d.johnson@gmail.com>
2023-03-30 23:40:08 -0700

    Create nvidia-cleanup.ps1

---
+++ nvidia-cleanup.ps1
@@ -0,0 +1,31 @@
+$path = "$env:ALLUSERSPROFILE\NVIDIA Corporation\Downloader"
+
+# This is the regex I use to look for old driver folders. The folder name MUST have at least 2 numbers in it as well as any number of hexadecimal digits.
+$filter = [regex] "^[0-9a-f]*\d[0-9a-f]*\d[0-9a-f]*(_extracted)?_?$"
+
+echo "Cleaning up Nvidia's garbage..."
+
+echo ""
+echo "  - Looking for old driver installers to delete..."
+Get-ChildItem -Path $path |
+    Where-Object -Property Name -match $filter |
+    Group-Object -Property Name |
+    ForEach-Object {
+        echo  "  - Recycling [$path\$($_.Group.Name)]..."
+        Remove-Item "$path\$($_.Group.Name)" -Recurse
+    }
+echo ""
+
+echo "  - Recycling $path\status.json..."
+Remove-Item "$path\status.json"
+
+echo "  - Creating empty $path\status.json..."
+'[]' > "$path\status.json"
+
+echo ""
+
+echo "Done!"
+
+echo ""
+
+pause
